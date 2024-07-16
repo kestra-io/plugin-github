3c736fc21ecfc0481b5c4c81a9d84a582c60766c
Nikita Hrytsenko <82543287+iNikitaGricenko@users.noreply.github.com>
2024-07-16 14:59:35 +0300

    feat: Added missing icons and package-info (#9)
    * Implement github plugin
    
    * Implement github plugin
    
    * fix: Apply suggestions from code review
    
    * Added package-info.java and icons
    
    ---------
    
    Co-authored-by: Loïc Mathieu <loikeseke@gmail.com>

---
+++ src/main/java/io/kestra/plugin/github/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using a GitHub API.",
+    categories = PluginSubGroup.PluginCategory.OTHER
+)
+package io.kestra.plugin.github;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
