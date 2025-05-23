daf74a263ebcdb35ab36c6b5764619e7ab1e1691
François Delbrayelle <fdelbrayelle@gmail.com>
2025-05-23 14:31:25 +0200

    fix(github): rename Dispatch task to RunWorkflow (#46)

--- src/test/java/workflows/RunWorkflowTest.java
+++ src/test/java/workflows/RunWorkflowTest.java
@@ -3,28 +3,28 @@ package workflows;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.runners.RunContextFactory;
-import io.kestra.plugin.github.workflows.Dispatch;
+import io.kestra.plugin.github.workflows.RunWorkflow;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @KestraTest
 @Disabled("disabled for ci/cd, as there unit tests requires secret (oauth) token")
-public class DispatchTest {
+public class RunWorkflowTest {
     @Inject
     private RunContextFactory runContextFactory;
 
     @Test
-    void testDispatch() throws Exception {
+    void testRunWorkflow() throws Exception {
         var runContext = runContextFactory.of();
 
-        var dispatchTask = Dispatch.builder()
+        var runWorkflowTask = RunWorkflow.builder()
             .oauthToken(Property.of(""))
             .repository(Property.of("kestra-io/plugin-github"))
             .workflowId(Property.of("105842276")) // https://api.github.com/repos/kestra-io/plugin-github/actions/workflows/105842276
             .ref(Property.of("master"))
             .build();
 
-        dispatchTask.run(runContext);
+        runWorkflowTask.run(runContext);
     }
 }
