b91c8a091eb1f5ac1d43e9baccc5e970ce506fd6
Malaydewangan09 <malaydewangan310@gmail.com>
2025-09-15 15:34:23 +0530

    fix(test): enable test

--- src/test/java/io/kestra/plugin/github/AbstractGithubTest.java
+++ src/test/java/io/kestra/plugin/github/AbstractGithubTest.java
@@ -3,13 +3,13 @@ package io.kestra.plugin.github;
 import com.google.common.base.Strings;
 
 public abstract class AbstractGithubTest {
-    private static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
+    private static final String GITHUB_OAUTH_TOKEN = System.getenv("GITHUB_OAUTH_TOKEN");
 
     protected static boolean isTokenMissing() {
         return Strings.isNullOrEmpty(getToken());
     }
 
     protected static String getToken() {
-        return GITHUB_TOKEN;
+        return GITHUB_OAUTH_TOKEN;
     }
 }
