949ef2dcdfe1688abd883ba333e0d7bc3b6125ba
Ludovic DEHON <tchiot.ludo@gmail.com>
2026-03-11 19:14:02 +0100

    fix wrong token

--- .github/setup-unit.sh
+++ .github/setup-unit.sh
@@ -1,2 +1,2 @@
 echo "github:" > src/test/resources/application-test.yml
-echo "  token: ${SLACK_BOT_TOKEN}" >> src/test/resources/application-test.yml
+echo "  token: ${GH_PERSONAL_TOKEN}" >> src/test/resources/application-test.yml
